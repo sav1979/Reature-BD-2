bf48a214669798e5fe5540029bfaa98536a0c6ab
SAV <hjiybxe@mail.ru>
2022-02-21 10:35:16 +0400

    1

--- ДЗ по БД №2.sql
+++ ДЗ по БД №2.sql
@@ -1,25 +1,25 @@
 create table if not exists List_executors (
 	executors id primary key, 
 	nickname varchar(40) not null, 
-	genre id references List genres(genre_id) 
+	genre id INTEGER REFERENCES genre(genre_id) NOT NULL 
 );
 
 create table if not exists List albums (
-	executors_id primary key not null references List_executors (executors id), 
-	album_id varchar(40) unique not null,
-	album_name text not null,
-	year_of_release date
+    album_id serial PRIMARY KEY,
+	executors_id INTEGER REFERENCES List_executors (executors id), 
+	album_name VARCHAR(40) NOT NULL,
+	year_of_release INTEGER NOT NULL
 );
 
 create table if not exists List_tracks (
-	album_id primary key not null references List_albums (album_id), 
-	track_id text unique not null,
-	title_track text not null,
-	tame numeric
+    track_id SERIAL PRIMARY KEY,
+	album_id INTEGER REFERENCES List_albums (album_id) not null, 
+	title_track VARCHAR(50) NOT NULL,
+	tame INTEGER NOT NULL,
 	);
-	
 	
 create table if not exists List_of_genres (
 	genre_id primary key,
-	genre_name text not null
+	genre_name VARCHAR(50) not null
 	);
+	
